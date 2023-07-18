791c2b83250d8c1918e1ea3af57fb7f5042449c6
Cyntaelis <67953493+Cyntaelis@users.noreply.github.com>
2023-07-18 09:28:14 -0400

    Change to wide layout

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(layout="wide")
 df = pd.read_csv("./tome_raid.csv", header=0)
-
 st.title("Raid Gear BiS Checker")
 st.subheader("Check = Raid Gear")
 st.dataframe(df)
