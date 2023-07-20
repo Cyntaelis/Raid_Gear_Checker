f255f5048dd2999e4f9dfb236fbfc5bb6c12193f
Cyntaelis <67953493+Cyntaelis@users.noreply.github.com>
2023-07-20 09:54:41 -0400

    Change icon

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="BiS Checker",
+    page_icon="crossed_swords",
+    layout="wide",
+)
 df = pd.read_csv("./tome_raid.csv", header=0)
 st.title("Raid Gear BiS Checker")
 st.subheader("Check = Raid Gear")
-st.dataframe(df)
 st.markdown("[Code available here](https://github.com/Cyntaelis/Raid_Gear_Checker/)", unsafe_allow_html=True)
+st.dataframe(df)
+
