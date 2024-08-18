06a56b1987b0fc0a0ab7c96f6585b0dcce9f23db
Ashleigh Manfred Ng Chan <q3a.katetsu@gmail.com>
2024-08-18 15:07:16 +0000

    updated for DT

--- updater.py
+++ updater.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 TOKEN = tokenfile.token
 TOME_RAID_VARS = ('head','body','hands','legs','feet','ears','neck','wrists')
-RAID_GEAR_NAME = "Ascension"
+RAID_GEAR_NAME = "Dark Horse"
 
 auth = coreapi.auth.TokenAuthentication(TOKEN, scheme='Bearer', domain='etro.gg')
 client = coreapi.Client(auth=auth)
@@ -13,12 +13,19 @@ schema = client.get("https://etro.gg/api/docs/")
 bis_data = client.action(schema, ["gearsets","bis"])
 
 datatable = []
+cache = {}
 for x in bis_data:
   row = [x["jobAbbrev"],x["name"]]
   for y in TOME_RAID_VARS:
-    row.append(RAID_GEAR_NAME in client.action(schema, ["equipment", "read"], params={"id":x[y]})["name"])
+    if x[y] in cache:
+      text = cache[x[y]]
+    else:
+      text = client.action(schema, ["equipment", "read"], params={"id":x[y]})["name"]
+      cache[x[y]] = text
+    row.append(RAID_GEAR_NAME in text)
+    print(text)
   datatable.append(row)
-  
+
 cols = ["Job","Set"]
 cols.extend(TOME_RAID_VARS)
 df = pd.DataFrame(datatable,columns=cols)
